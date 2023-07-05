c3f7272c5340fd60846f5db378fd750e5df7f1cd
Chika <chyka9632@gmail.com>
2023-07-05 19:02:34 +0100

    Finish the main and run the code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import glob
+import os
+
 import cv2
 import time
 from emailing import send_email
@@ -9,6 +11,14 @@ time.sleep(1)  # This gives the video time to load
 first_frame = None
 status_list = []
 count = 1
+
+
+# This fnx cleans unwanted images from the images folder
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 while True:
     status = 0
@@ -22,22 +32,22 @@ while True:
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
-    dil_frame = cv2.dilate(thresh_frame, None, iterations=2 )
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
     cv2.imshow("My videos", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        rect = cv2.rectangle(frame, (x, y), (x+w, x+h), (0, 255, 0), 3)
+        rect = cv2.rectangle(frame, (x, y), (x + w, x + h), (0, 255, 0), 3)
         if rect.any():
             status = 1
             cv2.imwrite(f"images/{count}.png", frame)
             count = count + 1
             all_images = glob.glob("images/*.png")
-            index = int(len(all_images)/2)
+            index = int(len(all_images) / 2)
             image_with_obj = all_images[index]
 
     status_list.append(status)
@@ -45,11 +55,10 @@ while True:
 
     if status_list[0] == 1 and status_list[1] == 0:
         send_email(image_with_obj)
+        clean_folder()
     print(status_list)
 
-
     cv2.imshow("Video", frame)
-
 
     key = cv2.waitKey(1)
 
