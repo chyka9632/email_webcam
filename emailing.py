024d1523c35ddb8bf2785cbf8f81ad63ba8fe294
Chika <chyka9632@gmail.com>
2023-07-05 18:45:31 +0100

    Finish the send_email function

--- emailing.py
+++ emailing.py
@@ -1,2 +1,28 @@
-def send_email():
-    print("Email sent")
+import imghdr
+import smtplib
+from email.message import EmailMessage
+
+SENDER = "curtis4ridaz@gmail.com"
+PASSWORD = "efeikwoyofwhhzzs"
+RECEIVER = "chyka9632@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Customer Alert"
+    email_message.set_content("Hey, a new customer just entered the shop")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/9.png")
