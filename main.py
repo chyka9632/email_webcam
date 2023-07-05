d8eceb780d98e56ca3ca1b6679d1d9cb2e82dac9
Chika <chyka9632@gmail.com>
2023-07-05 10:01:59 +0100

    Add timer

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import cv2
+import time
 
 video = cv2.VideoCapture(0)
-check, frame = video.read()
 
-print(check)
-print(frame)
+check1, frame1 = video.read()
+time.sleep(1)  # Adding timer
+
+
+check2, frame2 = video.read()
+time.sleep(1)
+
+check3, frame3 = video.read()
+time.sleep(1)
+
+
+print(frame3)
