d868b97ae8405150120be8e5dc5e17990384ed1f
Chika <chyka9632@gmail.com>
2023-07-05 14:43:22 +0100

    Define a send email fnx and import to main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)  # This gives the video time to load
 
 first_frame = None
-
+status_list = []
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Converting all the pixels to gray
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -26,7 +28,17 @@ while True:
         if cv2.contourArea(contour) < 10000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, x+h), (0, 255, 0), 3)
+        rect = cv2.rectangle(frame, (x, y), (x+w, x+h), (0, 255, 0), 3)
+        if rect.any():
+            status = 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+    print(status_list)
+
 
     cv2.imshow("Video", frame)
 
